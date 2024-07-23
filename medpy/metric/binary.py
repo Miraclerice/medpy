216df215e0c6215b95c594ba53ad8c33a3987b21
Miraclerice <1669176771@qq.com>
2024-07-23 13:49:49 +0800

    medpy/metric/binary.py func assd change

--- medpy/metric/binary.py
+++ medpy/metric/binary.py
@@ -468,10 +468,10 @@ def assd(result, reference, voxelspacing=None, connectivity=1):
     and then averaging the two lists. The binary images can therefore be supplied in any order.
     """
     assd = numpy.mean(
-        numpy.concatenate(
+        numpy.concatenate((
             __surface_distances(result, reference, voxelspacing, connectivity),
             __surface_distances(reference, result, voxelspacing, connectivity),
-        )
+        ))
     )
     return assd
 
